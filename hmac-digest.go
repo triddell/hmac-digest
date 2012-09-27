950c7c48adfa47a34a76166765a823f93be38489
Tim Riddell <tim@riddell.us>
2012-09-27 15:25:40 -0500

    Initial commit

---
+++ hmac-digest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"hash"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: hmac-digest [textToEncrypt] [secretKey]\n")
+		os.Exit(1)
+	}
+
+	var h hash.Hash = hmac.New(sha1.New, []byte(args[1]))
+
+	h.Write([]byte(args[0]))
+
+	fmt.Println(base64.URLEncoding.EncodeToString(h.Sum(nil)))
+}
