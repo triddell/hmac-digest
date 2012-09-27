9cc0953c3ed472ebb4343c2c787a6db117b82c33
Tim Riddell <tim@riddell.us>
2012-09-27 15:50:46 -0500

    Modify usage string

--- hmac-digest.go
+++ hmac-digest.go
@@ -12,17 +12,14 @@ import (
 
 func main() {
 	flag.Parse()
-
 	args := flag.Args()
 
 	if len(args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: hmac-digest [textToEncrypt] [secretKey]\n")
+		fmt.Fprintf(os.Stderr, "usage: hmac-digest [textToEncode] [secretKey]\n")
 		os.Exit(1)
 	}
 
 	var h hash.Hash = hmac.New(sha1.New, []byte(args[1]))
-
 	h.Write([]byte(args[0]))
-
 	fmt.Println(base64.URLEncoding.EncodeToString(h.Sum(nil)))
 }
